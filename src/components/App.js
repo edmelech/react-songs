0e6217ec10074f8cc89ab77d2c76376c26f65a1e
Ed Melech <mr.melech@gmail.com>
2021-08-31 16:12:36 +0100

    add padding top

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import SongList from './SongList';
 import SongDetail from './SongDetail';
+import Styling from './Styling.css';
 
 const App = () => {
   return (
-    <div className="ui container grid">
-      <div className="ui row">
-        <div className="column eight wide">
-          <SongList />
-        </div>
-        <div className="colum eight wide">
-          <SongDetail />
+    <div className="customcontainer">
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+          <div className="colum eight wide">
+            <SongDetail />
+          </div>
         </div>
       </div>
     </div>
